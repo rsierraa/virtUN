ea303a65874007970c9f78405e3f3c769d270456
Rod <rsierraa@unal.edu.co>
2023-10-25 18:15:38 -0500

    pagination tsx first commit

---
+++ src/components/PaginationBar.tsx
@@ -0,0 +1,29 @@
+import Link from "next/link";
+
+interface PaginationBarProps {
+    currentPage: number;
+    totalPages: number;
+    onPageChange: (page: number) => void;
+}
+
+
+export default function PaginationBar({currentPage, totalPages}: PaginationBarProps){
+    const maxPage = Math.min(totalPages, Math.max(currentPage + 4, 10));
+    const minPage = Math.max(1, Math.min(currentPage - 4, totalPages - 9));
+
+    const numberedPageItems: JSX.Element[] = []
+    
+    for(let page = minPage; page <= maxPage; page++){
+        numberedPageItems.push(
+            <Link
+            href={"?page=" + page}
+            key={page}
+            className={`join-item btn ${currentPage === page ? "btn-active pointer-events-none" : ""
+            }`}
+            >
+                {page}
+            </Link>
+        )
+
+    }
+}
