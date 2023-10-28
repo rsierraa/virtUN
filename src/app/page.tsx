247561ac035009f7613009c009dd6c0a7d9e60bf
NicolasLopez147 <nilopezn@unal.edu.co>
2023-10-28 09:17:07 -0500

    Merge both branches

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -29,33 +29,9 @@ export default async function Home({searchParams : {page = "1"}}: HomeProps) {
     take: pageSize,
   });
   return (
-    <div style={{ padding: "138px" }}>
+    <div className="flex flex-col items-center">
       <div className="hero rounded-xl bg-base-200">
-        <div className="flex-col hero-content lg:flex-row">
-          <Image
-            src={products[0].imageUrl}
-            alt={products[0].name}
-            width={400}
-            height={800}
-            className="w-full max-w-sm rounded-lg shadow-2xl"
-            priority
-          />
-          <div>
-            <h1 className="text-5xl font-bold">{products[0].name}</h1>
-            <p className="py-6">{products[0].description}</p>
-            <Link
-              href={"/products/" + products[0].id}
-              className="btn-primary btn"
-            >
-              Comprar
-            </Link>
-          </div>
-        </div>
-      </div>
-      <div className="grid grid-cols-1 gap-4 my-4 md:grid-cols-2 xl:grid-cols-3">
-        {products.slice(1).map((product) => (
-          <ProductCard product={product} key={product.id} />
-        ))}
+        <Category products={products} />
       </div>
       {totalPages > 1 && (
       <PaginationBar currentPage={currentPage} totalPages={totalPages} onPageChange={function (page: number): void {
