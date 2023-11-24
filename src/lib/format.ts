d0fd56e3234f68ecc34fd856db8e4e81338820d9
Rodrigo Sierra <rod@Rodrigos-MacBook-Air.local>
2023-11-24 13:02:35 -0500

    project rebasing virtun2.0

--- src/lib/format.ts
+++
@@ -1,8 +0,0 @@
-//format the price to COP (colombian pesos) with no decimals
-export function formatPrice(price: number) {
-  return price.toLocaleString("es-CO", {
-    style: "currency",
-    currency: "COP",
-    minimumFractionDigits: 0,
-  });
-}
