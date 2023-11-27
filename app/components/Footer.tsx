7366ab3f6a9241c2ff5057d2447eb54721154b37
NicolasLopez147 <nilopezn@unal.edu.co>
2023-11-27 15:24:36 -0500

    Fix bug

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,15 +1,15 @@
 import Link from "next/link";
-import Container from "../Container";
+import Container from "./Container";
 import FooterList from "./FooterList";
 import { AiFillGithub, AiFillInstagram } from "react-icons/ai";
 
 const Footer = () => {
   return (
-    <footer className="bg-slate-700 text-slate-200 text-sm mt-16">
+    <footer className="mt-16 text-sm bg-slate-700 text-slate-200">
       <Container>
-        <div className="flex flex-col md:flex-row justify-between pt-16 pb-8">
+        <div className="flex flex-col justify-between pt-16 pb-8 md:flex-row">
           <FooterList>
-            <h3 className="text-base font-bold mb-2">Shop Categories</h3>
+            <h3 className="mb-2 text-base font-bold">Shop Categories</h3>
             <Link href="#">SmartPhone</Link>
             <Link href="#">Portátiles</Link>
             <Link href="#">Pc de escritorio</Link>
@@ -18,13 +18,13 @@ const Footer = () => {
             <Link href="#">Accesorios</Link>
           </FooterList>
           <FooterList>
-            <h3 className="text-base font-bold mb-2">Atención al Cliente</h3>
+            <h3 className="mb-2 text-base font-bold">Atención al Cliente</h3>
             <Link href="#">Contáctanos</Link>
             <Link href="#">FAQs</Link>
             <Link href="#">Política de Datos</Link>
           </FooterList>
-          <div className="w-full md:w-1/3 mb-6 md:mb-0">
-            <h3 className="text-base font-bold mb-2">Acerca de Nosotros</h3>
+          <div className="w-full mb-6 md:w-1/3 md:mb-0">
+            <h3 className="mb-2 text-base font-bold">Acerca de Nosotros</h3>
             <p className="mb-2">
               Somos tu chaza virtual de tecnología, al mejor precio, dentro de la Nacho
             </p>
@@ -33,7 +33,7 @@ const Footer = () => {
             </p>
           </div>
           <FooterList>
-          <h3 className="text-base font-bold mb-2">
+          <h3 className="mb-2 text-base font-bold">
                         Síguenos en
                     </h3>
                     <div className="flex space-x-2">
